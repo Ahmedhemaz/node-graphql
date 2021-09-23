e63684702877b3d176c15d07cecf78c24c7876ac
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-09-23 17:32:49 +0200

    create hello types and resolvers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import { GraphQLServer } from "graphql-yoga";
+
+// Types
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+// Resolvers
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello World First Query!!!",
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("Server is running on localhost:4000");
+});
