9aaa37d0189865885fde64a82cafbfd6b29b1495
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-09-26 13:53:49 +0200

    sub on postupdate

--- src/mutations/posts/update-post.js
+++ src/mutations/posts/update-post.js
@@ -1,8 +1,12 @@
 import { getPostById } from "../../queries/posts";
+import { POST_CREATION, POST_STATES } from "../../subscriptions/posts";
 
-const updatePost = (db, args) => {
+//TODO Refactor update ifs logic
+const updatePost = (ctx, args) => {
   const { id, data } = args;
+  const { db, pubsub } = ctx;
   const post = getPostById(db.posts, id);
+  const oldPost = { ...post };
   if (!post) throw new Error("Post Does Not Exist");
   if (typeof data.title === "string") {
     post.title = data.title;
@@ -12,7 +16,31 @@ const updatePost = (db, args) => {
   }
   if (typeof data.published === "boolean") {
     post.published = data.published;
+
+    if (oldPost.published && !post.published) {
+      pubsub.publish(POST_CREATION, {
+        post: {
+          mutation: POST_STATES.DELETED,
+          data: oldPost,
+        },
+      });
+    } else if (!oldPost.published && post.published) {
+      pubsub.publish(POST_CREATION, {
+        post: {
+          mutation: POST_STATES.CREATED,
+          data: post,
+        },
+      });
+    }
+  } else if (post.published) {
+    pubsub.publish(POST_CREATION, {
+      post: {
+        mutation: POST_STATES.UPDATED,
+        data: post,
+      },
+    });
   }
+
   return post;
 };
 
