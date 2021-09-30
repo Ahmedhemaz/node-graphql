8beeb0b4d7564ca33e807f06c7baddcd8ff0609c
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-09-30 15:23:01 +0200

    add comments with prisma

--- src/resolvers/Comment.js
+++ src/resolvers/Comment.js
@@ -1,9 +1,10 @@
 import { getUserById } from "../queries/users/";
 import { getPostById } from "../queries/posts";
 
+// cause we are using prisma we don't need to write the fetching of relational data
 const Comment = {
-  author: (parent, args, { db }, info) => getUserById(db.users, parent.author),
-  post: (parent, args, { db }, info) => getPostById(db.posts, parent.postId),
+  // author: (parent, args, { db }, info) => getUserById(db.users, parent.author),
+  // post: (parent, args, { db }, info) => getPostById(db.posts, parent.postId),
 };
 
 export { Comment };
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -9,7 +9,7 @@ const Query = {
   post: (parent, args, { db }, info) => db.posts[0],
   users: (parent, args, ctx, info) => filterWithUserNameContains({ parent, args, ctx, info }),
   posts: (parent, args, ctx, info) => filterPostsContainsTestKeyWord({ parent, args, ctx, info }),
-  comments: (parent, args, ctx, info) => ctx.prisma.comments,
+  comments: (parent, args, ctx, info) => ctx.prisma.query.comments(null, info),
 };
 
 export { Query };
